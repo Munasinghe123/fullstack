1780f20de675424b9359318e4d12b5a42a314478
Munasinghe123 <jsan39196@gmail.com>
2024-08-08 11:21:56 +0530

    report_generation

--- frontend/src/components/UserDetails/UserDetails.jsx
+++ frontend/src/components/UserDetails/UserDetails.jsx
@@ -1,37 +1,46 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react"; // Import useRef here
 import Nav from "../Nav/Nav";
 import axios from "axios";
 import DisplayUser from "../DisplayUser/DisplayUser";
+import { useReactToPrint } from "react-to-print";
 
-//url to get the data from the database
+// URL to get the data from the database
 const URL = "http://localhost:5000/users";
 
-//fetching the data from the url
+// Fetching the data from the URL
 const fetchHandler = async () => {
   return await axios.get(URL).then((res) => res.data);
 };
 
 function UserDetails() {
-  const [users, setUsers] = useState();
+  const [users, setUsers] = useState([]); // Initialize as an empty array
+  const componentsRef = useRef(); // Use useRef to create a reference
 
   useEffect(() => {
-    fetchHandler().then((data) => setUsers(data.users));
+    fetchHandler().then((data) => setUsers(data.users || data)); // Adjust based on your API response
   }, []);
 
- 
+  // Report generation
+  const handlePrint = useReactToPrint({
+    content: () => componentsRef.current, // Reference to the element you want to print
+    documentTitle: "Users Report", // Use camel case here
+    onAfterPrint: () => alert("Users report successfully downloaded"), // Use camel case here
+  });
 
   return (
     <div>
-      <Nav></Nav>
-      <h1>user details</h1>
-      <div>
-        {users &&
-          users.map((user, id) => (
-            <div key={id}>
-              <DisplayUser user={user} />
-            </div>
-          ))}
+      <Nav />
+      <h1>User Details</h1>
+      <div ref={componentsRef}>
+        {/* Use componentsRef here */}
+        {users.map((user) => (
+          <div key={user._id}>
+            <DisplayUser user={user} />
+          </div>
+        ))}
       </div>
+      <button onClick={handlePrint}>Download Report</button>
+      {/* Uncomment to enable printing */}
     </div>
   );
 }
